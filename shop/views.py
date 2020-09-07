5e39bc1bf11e1fbbe951bd1976c5ebdc2f966de5
Denis <pdef8085@gmail.com>
2020-09-07 23:21:51 +0300

    make 2 pages home and about

--- shop/views.py
+++ shop/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
